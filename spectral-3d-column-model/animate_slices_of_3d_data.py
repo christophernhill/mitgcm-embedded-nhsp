428da93277ef9cea81157af792b5a824660a60a9
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2018-08-23 11:19:27 -0400

    Script to plot/animate MITGCM fields from C.

--- spectral-3d-column-model/animate_slices_of_3d_data.py
+++ spectral-3d-column-model/animate_slices_of_3d_data.py
@@ -4,42 +4,25 @@ import matplotlib.animation as animation
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument('-in', '--in-file', dest='in_filename', help='Input array binary file.')
-parser.add_argument('-out', '--out-file', dest='out_filename', help='Output array binary file.')
-parser.add_argument('-rec', '--rec-file', dest='rec_filename', help='Reconstructed array binary file.')
+parser.add_argument('-f', '--file', dest='filename', help='Input file.')
 args = parser.parse_args()
 
-Nx, Ny, Nz = 32, 32, 32
+Nx, Ny, Nz = 100, 100, 50
 record_length = 8  # [bytes]
 
-with open(args.in_filename, 'rb') as f:
+with open(args.filename, 'rb') as f:
     data = np.fromfile(f, dtype='f8', count=Nx*Ny*Nz)
-    in_array = np.reshape(data, [Nx, Ny, Nz], order='C')
+    a = np.reshape(data, [Nz, Ny, Nx], order='C')
 
-with open(args.out_filename, 'rb') as f:
-    data = np.fromfile(f, dtype='f8', count=Nx*Ny*Nz)
-    out_array = np.reshape(data, [Nx, Ny, Nz], order='C')
-
-with open(args.rec_filename, 'rb') as f:
-    data = np.fromfile(f, dtype='f8', count=Nx*Ny*Nz)
-    rec_array = np.reshape(data, [Nx, Ny, Nz], order='C')
-
-out_array = np.flip(np.flip(np.flip(out_array, 0), 1), 2)  # Flip the wavenumber array.
-rec_array = (rec_array / (2*Nx * 2*Ny * 2*Nz))  # Scaling reconstruction due to unnormalized IFFT.
-residual_array = in_array - rec_array
-
-array_names = ['input array', 'output array', 'reconstructed array', 'residual array']
-
-for i, a in enumerate([in_array, out_array, rec_array, residual_array]):
-    print('{:s}: shape={:}'.format(array_names[i], a.shape))
-    print('stats: mean={:g}, min={:g}, max={:g}, argmin={:}, argmax={:}'
-        .format(a.mean(), a.min(), a.max(),
-            np.unravel_index(a.argmin(), a.shape), np.unravel_index(a.argmax(), a.shape)))
-    print('')
+print('array: shape={:}'.format(a.shape))
+print('stats: mean={:g}, min={:g}, max={:g}, argmin={:}, argmax={:}'
+    .format(a.mean(), a.min(), a.max(),
+        np.unravel_index(a.argmin(), a.shape), np.unravel_index(a.argmax(), a.shape)))
+print('')
 
 fig, ax = plt.subplots()
 
-im = ax.imshow(rec_array[0], vmin=-1, vmax=1, animated=True)
+im = ax.imshow(a[0,:,:], vmin=-1e-4, vmax=1e-4, cmap='RdBu', animated=True)
 plt.colorbar(im)
 
 time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes,
@@ -49,7 +32,7 @@ def init_fig():
     pass
 
 def animate_fig(i):
-    im.set_array(rec_array[i])
+    im.set_array(a[i,:,:])
     time_text.set_text('lvl = {:d}'.format(i))
     return im, ax
 
